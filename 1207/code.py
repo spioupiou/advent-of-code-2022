5cf5fe4e198fe724cccc6259b696e322f28cfb0c
Cédrine Monnet <monnetcedrine@team-lab.com>
2023-11-07 17:42:07 +0900

    recursioooon

--- 1207/code.py
+++ 1207/code.py
@@ -1,46 +1,75 @@
 import re
-from collections import defaultdict 
 
-with open('test_input.txt') as f:
-  lines = f.readlines()
+with open("test_input.txt") as f:
+    lines = f.readlines()
+
 
 class Node:
-  def __init__(self, is_dir, path, size = 0, content = []):
-    self.is_dir = is_dir
-    self.path = path
-    self.size = size
-    self.content = content
-  
-  def add_content(self, content):
-    self.content.append(content)
+    def __init__(self, is_file, path, parent, size=0, content=[]):
+        self.is_file = is_file
+        self.path = path
+        self.parent = parent
+        self.size = size
+        self.content = content
 
-current_folder = Node(True, '/')
+    def add_content(self, folder):
+        self.content.append(folder)
+
+    def get_size(self):
+        if self.is_file:
+            return self.size
+        size = self.size
+        for sub in self.content:
+            size += sub.get_size()
+        self.size = size
+        return self.size
+
+
+root = Node(is_file=False, path="/", parent=None)
+current_folder = root
 
 for i, _ in enumerate(lines):
-  mv_up = re.match(r'\$\scd\s\.\.', lines[i])
-  if mv_up:
-    current_folder.path = re.sub(r'\/[a-zA-Z]+\/$', '/', current_folder.path)
-    continue
+    # move_up
+    move_up_cmd = re.match(r"\$\scd\s\.\.", lines[i])
+    if move_up_cmd:
+        current_folder = current_folder.parent
 
-  cmd = re.match(r'\$\scd\s([a-zA-Z]+|\/)', lines[i])
-  if cmd and cmd.group(1) != current_folder.path:
-    current_folder = Node(True, current_folder.path + cmd.group(1) + "/")
-    continue
-  
-  # file starts with digits (size)
-  file = re.match(r'(\d+)\s(.+)', lines[i])
-  if file:
-    current_folder.add_content(Node(False, current_folder.path + file.group(2), file.group(1)))
-    continue
+    # move_down
+    move_down_cmd = re.match(r"\$\scd\s([a-zA-Z]+)", lines[i])
+    if move_down_cmd:
+        current_path = current_folder.path + move_down_cmd.group(1) + "/"
+        for item in current_folder.content:
+            if item.path == current_path:
+                current_folder = item
+                break
 
-  sub_folder = re.match(r'dir\s([a-zA-Z]+)', lines[i])
-  if sub_folder:
-    current_folder.add_content(Node(True, current_folder.path + sub_folder.group(1) + "/"))
+    # add file to current_folder (file starts with their size)
+    file = re.match(r"(\d+)\s(.+)", lines[i])
+    if file:
+        file_size, file_name = int(file.group(1)), file.group(2)
+        current_folder.add_content(
+            Node(
+                is_file=True,
+                path=current_folder.path + file_name,
+                size=file_size,
+                parent=current_folder,
+            )
+        )
+        continue
+
+    # add sub_folder to current_folder
+    sub_folder = re.match(r"dir\s([a-zA-Z]+)", lines[i])
+    if sub_folder:
+        current_folder.add_content(
+            Node(
+                is_file=False,
+                path=current_folder.path + sub_folder.group(1) + "/",
+                content=[],
+                parent=current_folder,
+            )
+        )
 
 total = 0
-for node in current_folder.content:
-  if not node.is_dir:
-    print(node.path)
-    total += int(node.size)
-
+for item in root.content:
+    total += item.get_size()
 print(total)
