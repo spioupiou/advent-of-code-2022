80b5a1ee10b3a8ed4110df93b38606951a1fc45a
Cédrine Monnet <monnetcedrine@team-lab.com>
2023-11-11 00:00:59 +0900

    variable names

--- 1210/part_2.py
+++ 1210/part_2.py
@@ -1,30 +1,27 @@
-with open('test_input.txt') as f:
-  lines = f.readlines()
+with open("input.txt") as f:
+    lines = f.readlines()
 
 x = 1
 x_values = [x]
 
 for line in lines:
-  command, *value = line.strip('\n').split(' ')
-  x_values.append(x)
-  if command == 'addx':
-    x += int(*value)
+    command, *value = line.strip("\n").split(" ")
     x_values.append(x)
+    if command == "addx":
+        x += int(*value)
+        x_values.append(x)
 
-stuff = []
-for i in range(0, len(x_values), 40):
-  stuff.append(x_values[i:i+40])
+# Make the 40*6 grid
+grid = []
+for i in range(0, len(x_values) - 1, 40):
+    grid.append(x_values[i : i + 40])
 
-for item in stuff:
-  for i in range(0, len(item)):
-    # print(item[i], end = "")
-    CRT = i
-    if i == 0:
-      CRT += 1
-    if item[i] -1 <= CRT <= item[i]+1:
-      print('#', end='')
-    else:
-      print('.', end ='')
-  
-  print('\n')
+for line in grid:
+    for i in range(0, len(line)):
+        CRT = i
+        if line[i] - 1 <= CRT <= line[i] + 1:
+            print("# ", end="")
+        else:
+            print(". ", end="")
 
+    print("\n")
