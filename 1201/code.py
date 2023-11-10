560ec880219ef0de18ecd51f2efba2da03a84562
Cédrine Monnet <monnetcedrine@team-lab.com>
2023-11-10 12:06:21 +0900

    refactor day 1

--- 1201/code.py
+++ 1201/code.py
@@ -2,25 +2,19 @@ with open('input.txt') as f:
   lines = f.readlines()
 
 inventories = []
-sum = 0
+total = 0
 for line in lines:
   line = line.strip('\n')
   if line == '':
-    inventories.append(sum)
-    sum = 0
+    inventories.append(total)
+    total = 0
   else:
-    sum += int(line)
+    total += int(line)
 
 inventories.sort(reverse=True)
 
 # TOP 1
 print(inventories[0])
 
-top_3_sum = 0
-for i, inventory in enumerate(inventories):
-  if i == 3:
-    break
-  top_3_sum += inventory
-
 # TOP 3
-print(top_3_sum)
+print(sum(inventories[0:3]))
