7f0faacc1c3a936f2e084af52957623c4c01fa7a
Cédrine Monnet <monnetcedrine@team-lab.com>
2023-11-14 17:13:25 +0900

    fix: error handling + store the path instead of no of step to avoid losing information

--- 1212/code.py
+++ 1212/code.py
@@ -50,39 +50,53 @@ class PointWithStep:
         self.step = step
 
 def find_shortest_path(start: Point, end: Point):
-    # Create a queue of points to visit + set of visited points
-    # Add the starting point to the queue and visited set
-    queue = [PointWithStep(start, 0)]
+    # Keep track of visited points
     visited = set([start])
+    # Keep track of all the paths to be checked
+    queue = [[start]]
     
     # Until the queue is emptied:
     while queue:
-        # Dequeue the first point
-        point_with_step = queue.pop(0)
-        current, step = point_with_step.point, point_with_step.step
+        # Dequeue the first path
+        path = queue.pop(0)
+        # Get the last point from the path
+        current = path[-1]
+        # If the last point has not been visited...
         if current.coordinates() not in visited:
             visited.add(current.coordinates())
-            # Find adjacent points
+            # ...find the adjacent points
             filtered_adjacent_points = []
             # Make sure the adjacent points are in the height map and can be moved to
             for point in current.find_neighbors():
                 if (point.coordinates()) in height_map and current.can_move_to(point):
                     filtered_adjacent_points.append(point)
-            # For each adjacent point:
+            # For each adjacent point, make a new path and push it into the queue:
             for point in filtered_adjacent_points:
-                # If the point is the target point, return true (increment step by 1 because we need to move to the goal)
+                new_path = list(path)
+                new_path.append(point)
+                queue.append(new_path)
+                # If the point is the target point, return the path taken to reach it
                 if point.is_goal():
-                    return step + 1
-                # Otherwise increment the step needed to reach that point by 1 and append to queue
-                queue.append(PointWithStep(point, step + 1))
+                    return new_path
+                
+    raise ValueError("No path found")
 
 # Part 1
-print(find_shortest_path(start, end))
+path = find_shortest_path(start, end)
+# Print len(path) - 1 to find the number of steps (because you don't need to move to the starting point)
+print(len(path)-1)
 
 # Part 2
 paths = []
 for k, v in height_map:
+    # Try all possible starting points with elevation 0
     if height_map[k, v] == 0:
-        paths.append(find_shortest_path(Point(k, v), end))
+        try:
+            path = find_shortest_path(Point(k, v), end)
+            paths.append(path)
+        except ValueError:
+            print("No path found, skipping")
+            continue
 
-print(min(filter(lambda x: x is not None, paths)))
+# print min no of steps
+print(min([len(path) for path in paths])-1)
