4f4d71706536eb487ffb19404bc8688549b7331b
Cédrine Monnet <monnetcedrine@team-lab.com>
2023-11-23 15:47:19 +0900

    changed the name to blocks

--- 1214/code.py
+++ 1214/code.py
@@ -20,12 +20,12 @@ def find_points_between(start, end: tuple) -> list:
 
     return points
 
-def create_rocks(input_file_name: str) -> list:
+def create_blocks(input_file_name: str) -> list:
     with open(input_file_name) as f:
         lines = f.readlines()
 
 
-    rocks = []
+    blocks = []
     for i, _ in enumerate(lines):
         line = lines[i].strip('\n')
         coordinates = re.findall(r"[^->\s]+", line)
@@ -36,36 +36,36 @@ def create_rocks(input_file_name: str) -> list:
 
             for point in find_points_between(start, end):
                 path.add(point)
-        rocks.extend(path)
+        blocks.extend(path)
 
-    return rocks    
+    return blocks    
 
 
-def add_sand(rocks: list, bottom_max: int) -> tuple:
+def add_sand(blocks: list, bottom_max: int) -> tuple:
     sand = (500, 0)
     while sand[1] <= bottom_max:
         # down
-        if (sand[0], sand[1]+1) not in rocks:
+        if (sand[0], sand[1]+1) not in blocks:
             sand = (sand[0], sand[1]+1)
         # left down
-        elif (sand[0]-1, sand[1]+1) not in rocks:
+        elif (sand[0]-1, sand[1]+1) not in blocks:
             sand = (sand[0]-1, sand[1]+1)
         # right down
-        elif (sand[0]+1, sand[1]+1) not in rocks:
+        elif (sand[0]+1, sand[1]+1) not in blocks:
             sand = (sand[0]+1, sand[1]+1)
         else: # sand is stuck
             return sand
         
-def part_1(rocks: list):
-    bottom_max = max(rocks, key=lambda p: p[1])[1]
+def part_1(blocks: list):
+    bottom_max = max(blocks, key=lambda p: p[1])[1]
     sand_units = 0
-    new_sand = add_sand(rocks=rocks, bottom_max=bottom_max)
+    new_sand = add_sand(blocks=blocks, bottom_max=bottom_max)
     while new_sand != None:
-        rocks.append((new_sand[0], new_sand[1]))
+        blocks.append((new_sand[0], new_sand[1]))
         sand_units +=1
-        new_sand = add_sand(rocks=rocks, bottom_max=bottom_max)
+        new_sand = add_sand(blocks=blocks, bottom_max=bottom_max)
     return sand_units        
 
 
-rocks = create_rocks(input_file_name="input.txt")
-print(part_1(rocks=rocks))
+blocks = create_blocks(input_file_name="input.txt")
+print(part_1(blocks=blocks))
